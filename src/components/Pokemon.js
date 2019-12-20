0a26cc12fde964c429937e0fab8473da7574a40b
Noelia Luque <n.luque91@gmail.com>
2019-12-20 10:15:51 +0100

    add css styles WIP

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -1,14 +1,19 @@
 import React from "react";
+import "../stylesheet/Pokemon.css";
 
 function Pokemon(props) {
   const types = props.pokemon.types;
   return (
     <li className="list-item">
-      <img src={props.pokemon.url} alt={props.pokemon.name} />
-      <p>{props.pokemon.name}</p>
-      <ul>
+      <img src={props.pokemon.url} alt={props.pokemon.name} className="item-img" />
+      <p className="item-name">{props.pokemon.name}</p>
+      <ul className="list-types">
         {types.map(function(type, index) {
-          return <li key={index}>{type}</li>;
+          return (
+            <li key={index} className="type">
+              {type}
+            </li>
+          );
         })}
       </ul>
     </li>
