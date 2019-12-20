6efd8cafa1e52770362734ccb97c9597be5e7ad8
Noelia Luque <n.luque91@gmail.com>
2019-12-20 08:31:37 +0100

    add react app and run build

---
+++ docs/precache-manifest.04868d3e5ea48a8239abbeacf45679b6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7d009cf70b429bdfe223a2230d4e14d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e12fc5f6ad43e3181772",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "f8554b8d92c1d9b22585",
+    "url": "/static/js/2.c79b2b03.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.c79b2b03.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e12fc5f6ad43e3181772",
+    "url": "/static/js/main.e68ad502.chunk.js"
+  },
+  {
+    "revision": "0c05771f0aab051a1cfe",
+    "url": "/static/js/runtime-main.1319eb95.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
